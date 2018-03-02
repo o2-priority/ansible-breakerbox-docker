e1c6c7d1db41bc6ade96a42225bddb916cc3b56c
Matthew Ralph <ralpmat@gmail.com>
2018-03-02 11:33:17 +0000

    PRI-702: Add more tests for postgres DB

--- test/integration/postgres-db/serverspec/default_spec.rb
+++ test/integration/postgres-db/serverspec/default_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe docker_container('breakerbox-postgres') do
-	it { should exist }
+  it { should exist }
+end
+
+# Check database created
+describe command('/usr/local/bin/psql.sh -c "\d"') do
+  its(:stdout) { should match /breakerbox.*postgres/ }
+end
+
+# Check initial tables created
+describe command('/usr/local/bin/psql.sh -c "\l"') do
+  its(:stdout) { should match /dependency_id_seq/ }
+  its(:stdout) { should match /databasechangelog/ }
+  its(:stdout) { should match /service/ }
 end
